9d28275bb64aa5376675dcc9995b16c6412592de
Harish-bolloju <51753713+Harish-bolloju@users.noreply.github.com>
2021-04-29 16:08:54 +0530

    Add files via upload
    new added

---
+++ Program (3).cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OOPS_DAY2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("******DateTime*********");
+            string dob = Console.ReadLine();
+            DateTime dt = DateTime.Parse(dob);
+            Console.WriteLine("Your date of birth is:{0}", dt);
+            Console.ReadLine();
+
+             
+        }
+        
+    }
+}
